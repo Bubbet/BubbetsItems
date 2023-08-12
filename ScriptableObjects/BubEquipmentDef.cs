d29b2cff099ec12aa1ac79212a23d0b831c3086f
Bubbet <bubbet@bubbet.dev>
2023-08-11 22:15:45 -0600

    Initial Commit

---
+++ ScriptableObjects/BubEquipmentDef.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using RoR2;
+using UnityEngine;
+
+namespace BubbetsItems
+{
+	[CreateAssetMenu(menuName = "BubbetsItemss/BubEquipmentDef")]
+	public class BubEquipmentDef : EquipmentDef
+	{
+		public GameObject displayModelPrefab;
+
+		private void OnValidate()
+		{
+			canDrop = true;
+		}
+
+		[ContextMenu("Bub Auto Populate Tokens")]
+		public new void AutoPopulateTokens()
+		{
+			string arg = name.ToUpperInvariant().Substring("EquipmentDef".Length);
+			nameToken = $"BUB_{arg}_NAME";
+			pickupToken = $"BUB_{arg}_PICKUP";
+			descriptionToken = $"BUB_{arg}_DESC";
+			loreToken = $"BUB_{arg}_LORE";
+		}
+	}
+}
---
+++ ScriptableObjects/BubsItemsContentPackProvider.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RoR2;
+using RoR2.ContentManagement;
+using RoR2.ExpansionManagement;
+using UnityEngine;
+
+namespace BubbetsItems
+{
+	
+	[CreateAssetMenu(menuName = "BubbetsItemss/ContentPackProvider")]
+	public class BubsItemsContentPackProvider : SerializableContentPack
+	{
+		public ExpansionDef[] expansionDefs = {};
+		public ItemRelationshipType[] itemRelationshipTypes = {};
+		public ItemRelationshipProvider[] itemRelationshipProviders = {};
+		public ItemTierDef[] itemTierDefs = {};
+
+		public override ContentPack CreateContentPack()
+		{
+			var content = base.CreateContentPack();
+			content.expansionDefs.Add(expansionDefs);
+			content.itemRelationshipTypes.Add(itemRelationshipTypes);
+			content.itemRelationshipProviders.Add(itemRelationshipProviders);
+			content.itemTierDefs.Add(itemTierDefs);
+			return content;
+		}
+	}
+}
